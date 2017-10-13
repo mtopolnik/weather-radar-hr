8680bd0ad0d237d9134d6c46e548212e7309ce45
Miljenko Stojicic <miljenko.stojicic@gmail.com>
2017-10-13 22:06:22 +0200

    App Skeleton

---
+++ app/src/main/java/com/belotron/weatherradarhr/RadarLisca.java
@@ -0,0 +1,13 @@
+package com.belotron.weatherradarhr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadarLisca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radar_lisca);
+    }
+}
