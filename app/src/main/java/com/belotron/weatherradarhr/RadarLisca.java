84b7e4c1734515f627ed09d04c23774bb6e5b95d
Miljenko Stojicic <miljenko.stojicic@gmail.com>
2017-10-16 21:53:11 +0200

    Add WebView with radar image

--- app/src/main/java/com/belotron/weatherradarhr/RadarLisca.java
+++ app/src/main/java/com/belotron/weatherradarhr/RadarLisca.java
@@ -2,6 +2,7 @@ package com.belotron.weatherradarhr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class RadarLisca extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class RadarLisca extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radar_lisca);
+        WebView wv = (WebView) findViewById(R.id.liscaWeb);
+        wv.loadUrl("http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif");
     }
 }
