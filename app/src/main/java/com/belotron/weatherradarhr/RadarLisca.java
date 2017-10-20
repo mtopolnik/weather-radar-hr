a38f213f086f32b69a6e940da2fab7ba848a4083
Miljenko Stojicic <miljenko.stojicic@gmail.com>
2017-10-20 21:20:42 +0200

    Use ImageView instead of WebView

--- app/src/main/java/com/belotron/weatherradarhr/RadarLisca.java
+++ app/src/main/java/com/belotron/weatherradarhr/RadarLisca.java
@@ -2,7 +2,10 @@ package com.belotron.weatherradarhr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import com.koushikdutta.ion.Ion;
 
 public class RadarLisca extends AppCompatActivity {
 
@@ -10,7 +13,9 @@ public class RadarLisca extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radar_lisca);
-        WebView wv = (WebView) findViewById(R.id.liscaWeb);
-        wv.loadUrl("http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif");
+        ImageView imgView = (ImageView) findViewById(R.id.imageView);
+        Ion.with(imgView)
+                .placeholder(R.drawable.rectangle)
+                .load("http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif");
     }
 }
