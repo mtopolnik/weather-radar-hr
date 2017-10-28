d7c40dc8438ff96499d3da292caa98ee69b71127
Miljenko Stojicic <miljenko.stojicic@gmail.com>
2017-10-28 20:58:18 +0200

    Go back to WebView

--- app/src/main/java/com/belotron/weatherradarhr/MainActivity.java
+++ app/src/main/java/com/belotron/weatherradarhr/MainActivity.java
@@ -1,7 +1,7 @@
 package com.belotron.weatherradarhr;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -9,46 +9,95 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.async.future.TransformFuture;
 import com.koushikdutta.ion.Ion;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import static com.belotron.weatherradarhr.ModifyGifFramerate.editGif;
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
+import static com.belotron.weatherradarhr.ModifyGifFramerate.modifyGifFramerate;
 
 public class MainActivity extends FragmentActivity {
 
-    static final SparseArray<TabDescriptor> tabs = new SparseArray<>(); static {
-        tabs.put(0, new TabDescriptor(
-                "Lisca",
-                "http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif"
-        ));
-        tabs.put(1, new TabDescriptor(
-                "Puntijarka-Bilogora-Osijek",
-                "http://vrijeme.hr/kradar-anim.gif"
-        ));
-        tabs.put(2, new TabDescriptor(
-                "Dubrovnik",
-                "http://vrijeme.hr/dradar-anim.gif"
-        ));
-    }
+    private static final TabDescriptor[] tabs = {
+            new TabDescriptor("Lisca",
+                    "http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif",
+                    10),
+            new TabDescriptor("Puntijarka-Bilogora-Osijek",
+                    "http://vrijeme.hr/kradar-anim.gif",
+                    15),
+            new TabDescriptor("Dubrovnik",
+                    "http://vrijeme.hr/dradar-anim.gif",
+                    15)
+    };
+
+    private static final int BUFSIZ = 512;
+    private static final String LOGTAG = "WeatherRadar";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
+        writeHtmlsForTabs();
         setContentView(R.layout.activity_main);
         ViewPager viewPager = findViewById(R.id.pager);
         viewPager.setAdapter(new FlipThroughRadarImages(getSupportFragmentManager()));
+    }
+
+    private void writeHtmlsForTabs() {
+        String htmlTemplate = readTemplate();
+        for (int i = 0; i < tabs.length; i++) {
+            File htmlFile = htmlFileForTab(i, getApplicationContext());
+            try (Writer w = new BufferedWriter(new FileWriter(htmlFile))) {
+                w.write(expandTemplate(htmlTemplate, tabs[i]));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static String expandTemplate(String htmlTemplate, TabDescriptor tab) {
+        Matcher m = Pattern.compile("\\$\\{([^}]+)\\}").matcher(htmlTemplate);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            switch (m.group(1)) {
+                case "title":
+                    m.appendReplacement(sb, tab.title);
+                    break;
+                case "imageFilename":
+                    m.appendReplacement(sb, tab.filename());
+                    break;
+                default:
+            }
+        }
+        m.appendTail(sb);
+        return sb.toString();
+    }
+
+    private String readTemplate() {
+        try (InputStream is = getApplicationContext().getAssets().open("radar_image.html")) {
+            BufferedReader r = new BufferedReader(new InputStreamReader(is));
+            char[] buf = new char[BUFSIZ];
+            StringBuilder b = new StringBuilder(BUFSIZ);
+            for (int count; (count = r.read(buf)) != -1;) {
+                b.append(buf, 0, count);
+            }
+            return b.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static File htmlFileForTab(int i, Context context) {
+        return new File(context.getNoBackupFilesDir(), "tab" + i + ".html");
     }
 
     private static class FlipThroughRadarImages extends FragmentPagerAdapter {
@@ -59,7 +108,7 @@ public class MainActivity extends FragmentActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return tabs.get(position).title;
+            return tabs[position].title;
         }
 
         @Override
@@ -84,28 +133,33 @@ public class MainActivity extends FragmentActivity {
                                  @Nullable ViewGroup container,
                                  @Nullable Bundle savedInstanceState
         ) {
-            final TabDescriptor desc = tabs.get(getArguments().getInt("index"));
-            Log.i("RadarImageFragment", desc.title);
+            final int tabIndex = getArguments().getInt("index");
+            final TabDescriptor desc = tabs[tabIndex];
+            Log.i(LOGTAG, desc.title);
             View rootView = inflater.inflate(R.layout.image_radar, container, false);
-            final ImageView imgView = rootView.findViewById(R.id.image_view_radar);
-            final File gifFile = new File(getContext().getNoBackupFilesDir(), desc.title + ".gif");
+            final WebView webView = rootView.findViewById(R.id.web_view_radar);
+            WebSettings s = webView.getSettings();
+            s.setSupportZoom(true);
+            s.setBuiltInZoomControls(true);
+            s.setDisplayZoomControls(false);
+            s.setUseWideViewPort(true);
+            s.setLoadWithOverviewMode(true);
+            final File gifFile = new File(getContext().getNoBackupFilesDir(), desc.filename());
             Ion.with(getContext())
                     .load(desc.url)
                     .asByteArray()
                     .setCallback(new FutureCallback<byte[]>() {
                         @Override
                         public void onCompleted(Exception e, byte[] result) {
-                            editGif(ByteBuffer.wrap(result), 10, 120, 200);
+                            modifyGifFramerate(result, (int) (1.2 * tabs[tabIndex].minutesPerFrame), 120, 200);
                             try (OutputStream out = new FileOutputStream(gifFile)) {
-                                Log.i("RadarImageFragment", "out.write(result)");
                                 out.write(result);
-                                Log.i("RadarImageFragment", "out.write(result) success");
                             } catch (IOException e1) {
                                 throw new RuntimeException(e1);
                             }
-                            Ion.with(RadarImageFragment.this)
-                                    .load(gifFile)
-                                    .intoImageView(imgView);
+                            String url = htmlFileForTab(tabIndex, getContext()).toURI().toString();
+                            Log.i(LOGTAG, url);
+                            webView.loadUrl(url);
                         }
                     });
             return rootView;
@@ -115,10 +169,16 @@ public class MainActivity extends FragmentActivity {
     private static final class TabDescriptor {
         final String title;
         final String url;
+        final int minutesPerFrame;
 
-        TabDescriptor(String title, String url) {
+        TabDescriptor(String title, String url, int minutesPerFrame) {
             this.title = title;
             this.url = url;
+            this.minutesPerFrame = minutesPerFrame;
+        }
+
+        String filename() {
+            return url.substring(url.lastIndexOf('/') + 1, url.length());
         }
     }
 }
--- app/src/main/java/com/belotron/weatherradarhr/ModifyGifFramerate.java
+++ app/src/main/java/com/belotron/weatherradarhr/ModifyGifFramerate.java
@@ -8,39 +8,44 @@ import static java.nio.ByteOrder.LITTLE_ENDIAN;
 @SuppressWarnings("MagicNumber")
 class ModifyGifFramerate {
 
-    static void editGif(ByteBuffer buf, int delayTime, int lastFrameHoldTime, int loopCount) {
+    private final ByteBuffer buf;
+
+    private ModifyGifFramerate(ByteBuffer buf) {
+        this.buf = buf;
+    }
+
+    static void modifyGifFramerate(byte[] bytes, int delayTime, int lastFrameHoldTime, int loopCount) {
+        new ModifyGifFramerate(ByteBuffer.wrap(bytes)).go(delayTime, lastFrameHoldTime, loopCount);
+    }
+
+    private void go(int delayTime, int lastFrameHoldTime, int loopCount) {
         buf.order(LITTLE_ENDIAN);
-        String gifVersion = getString(buf, 6);
+        String gifVersion = getString(6);
         if (!gifVersion.equals("GIF87a") && !gifVersion.equals("GIF89a")) {
             throw new AssertionError("Not a GIF file");
         }
-        skipLogicalScreenDescriptorAndGlobalColorTable(buf);
+        skipLogicalScreenDescriptorAndGlobalColorTable();
         while (buf.hasRemaining()) {
-            int blockType = nextByte(buf);
+            int blockType = nextByte();
             switch (blockType) {
                 case 0x21:
-                    int extensionLabel = nextByte(buf);
+                    int extensionLabel = nextByte();
                     switch (extensionLabel) {
                         case 0xf9:
-                            editGraphicControlExtension(buf, delayTime);
-                            break;
-                        case 0xfe:
-                            skipSubBlocks(buf);
+                            editGraphicControlExtension(delayTime);
                             break;
                         case 0xff:
-                            editApplicationExtension(buf, loopCount);
+                            editApplicationExtension(loopCount);
                             break;
                         default:
-                            throw new AssertionError(String.format("Unrecognized extension label: 0x%02x",
-                                    extensionLabel));
+                            skipSubBlocks();
                     }
                     break;
                 case 0x2c:
-                    skipImageDescriptor(buf);
+                    skipImageDescriptor();
                     break;
                 case 0x3b: // Trailer block, marks the end of GIF
                     buf.reset(); // restore the position of last frame's delay time
-                    System.out.format("Last frame hold time %d%n", lastFrameHoldTime);
                     buf.putChar((char) lastFrameHoldTime);
                     return;
                 default:
@@ -49,89 +54,85 @@ class ModifyGifFramerate {
         }
     }
 
-    private static void skipLogicalScreenDescriptorAndGlobalColorTable(ByteBuffer buf) {
-        skip(buf, 4); // canvas width and height
-        int packedFields = nextByte(buf);
-        skip(buf, 2); // Background Color Index and Pixel Aspect Ratio
-        skipColorTable(buf, packedFields);
-    }
-
-    private static void skipImageDescriptor(ByteBuffer buf) {
-        skip(buf, 8); // image position and size
-        int packedFields = nextByte(buf);
-        skipColorTable(buf, packedFields);
-        skip(buf, 1); // LZW Minimum Code Size
-        skipSubBlocks(buf);
-    }
-
-    private static void editApplicationExtension(ByteBuffer buf, int loopCount) {
-        int blockSize = nextByte(buf);
+    private void editApplicationExtension(int loopCount) {
+        int blockSize = nextByte();
         if (blockSize != 11) {
             throw new AssertionError("Invalid Application Extension block size: " + blockSize);
         }
-        String appId = getString(buf, 11);
+        String appId = getString(11);
         if (!appId.equals("NETSCAPE2.0")) {
-            skipSubBlocks(buf);
+            skipSubBlocks();
             return;
         }
-        int len = nextByte(buf);
-        int subBlockId = nextByte(buf);
+        int len = nextByte();
+        int subBlockId = nextByte();
         if (subBlockId == 1) {
             // Netscape Looping Extension
             if (len != 3) {
                 throw new AssertionError("Invalid Netscape Looping Extension block size: " + len);
             }
-            int currLoopCount = buf.getChar(buf.position()); // don't advance position
-            System.out.format("Loop Count %d -> %d%n", currLoopCount, loopCount);
             buf.putChar((char) loopCount);
-            int terminatorByte = nextByte(buf);
+            int terminatorByte = nextByte();
             if (terminatorByte != 0) {
                 throw new AssertionError("Invalid terminator byte " + terminatorByte);
             }
         }
     }
 
-    private static void editGraphicControlExtension(ByteBuffer buf, int delayTime) {
-        int blockSize = nextByte(buf);
+    private void editGraphicControlExtension(int delayTime) {
+        int blockSize = nextByte();
         if (blockSize != 4) {
             throw new AssertionError("Invalid Graphic Control Extension block size: " + blockSize);
         }
-        skip(buf, 1); // Packed Fields
-        int currDelayTime = buf.getChar(buf.position()); // don't advance position
+        skip(1); // Packed Fields
         buf.mark(); // will be used to modify the delay time of the final frame
-        System.out.format("Delay time %d -> %d%n", currDelayTime, delayTime);
         buf.putChar((char) delayTime);
-        skip(buf, 1); // transparentColorIndex
-        int blockTerminator = nextByte(buf);
+        skip(1); // transparentColorIndex
+        int blockTerminator = nextByte();
         if (blockTerminator != 0) {
             throw new AssertionError(String.format("Invalid block terminator byte: 0x%02x", blockTerminator));
         }
     }
 
-    private static int decodeColorTableSize(int packedFields) {
+    private void skipLogicalScreenDescriptorAndGlobalColorTable() {
+        skip(4); // canvas width and height
+        int packedFields = nextByte();
+        skip(2); // Background Color Index and Pixel Aspect Ratio
+        skipColorTable(packedFields);
+    }
+
+    private void skipImageDescriptor() {
+        skip(8); // image position and size
+        int packedFields = nextByte();
+        skipColorTable(packedFields);
+        skip(1); // LZW Minimum Code Size
+        skipSubBlocks();
+    }
+
+    private void skipColorTable(int packedFields) {
         boolean colorTablePresent = (packedFields & 0b1000_000) != 0;
-        return colorTablePresent ? 1 << ((packedFields & 0b111) + 1) : 0;
+        if (!colorTablePresent) {
+            return;
+        }
+        int colorTableSize = 1 << ((packedFields & 0b111) + 1);
+        skip(3 * colorTableSize);
     }
 
-    private static void skipColorTable(ByteBuffer buf, int packedFields) {
-        skip(buf, 3 * decodeColorTableSize(packedFields));
-    }
-
-    private static void skipSubBlocks(ByteBuffer buf) {
-        for (int len; (len = nextByte(buf)) != 0; ) {
-            skip(buf, len);
+    private void skipSubBlocks() {
+        for (int len; (len = nextByte()) != 0; ) {
+            skip(len);
         }
     }
 
-    private static void skip(ByteBuffer buf, int len) {
+    private void skip(int len) {
         buf.position(buf.position() + len);
     }
 
-    private static int nextByte(ByteBuffer buf) {
+    private int nextByte() {
         return ((int) buf.get()) & 0xff;
     }
 
-    private static String getString(ByteBuffer buf, int len) {
+    private String getString(int len) {
         buf.limit(buf.position() + len);
         try {
             return Charset.forName("ISO-8859-1").decode(buf.slice()).toString();
