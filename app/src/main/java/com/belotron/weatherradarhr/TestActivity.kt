f515ed483cf347bfe0208b9cad4a215dabc3ee1c
Marko Topolnik <marko.topolnik@gmail.com>
2018-02-26 19:02:49 +0100

    Test WIP

---
+++ app/src/main/java/com/belotron/weatherradarhr/TestActivity.kt
@@ -0,0 +1,22 @@
+package com.belotron.weatherradarhr
+
+import android.app.Activity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_test.*
+
+class TestActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+        digit0.setImageBitmap(LradarOcr.digitBitmaps[0])
+        digit1.setImageBitmap(LradarOcr.digitBitmaps[1])
+        digit2.setImageBitmap(LradarOcr.digitBitmaps[2])
+        digit3.setImageBitmap(LradarOcr.digitBitmaps[3])
+        digit4.setImageBitmap(LradarOcr.digitBitmaps[4])
+        digit5.setImageBitmap(LradarOcr.digitBitmaps[5])
+        digit6.setImageBitmap(LradarOcr.digitBitmaps[6])
+        digit7.setImageBitmap(LradarOcr.digitBitmaps[7])
+        digit8.setImageBitmap(LradarOcr.digitBitmaps[8])
+        digit9.setImageBitmap(LradarOcr.digitBitmaps[9])
+    }
+}
