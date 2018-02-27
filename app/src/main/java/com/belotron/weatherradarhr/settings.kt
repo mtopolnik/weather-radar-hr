f073c8bbeadaffd02792a8853c5a8befbc50a4b9
Marko Topolnik <marko.topolnik@gmail.com>
2018-02-27 19:40:24 +0100

    Add full-screen flag to the Settings activity

--- app/src/main/java/com/belotron/weatherradarhr/settings.kt
+++ app/src/main/java/com/belotron/weatherradarhr/settings.kt
@@ -3,10 +3,12 @@ package com.belotron.weatherradarhr
 import android.app.Activity
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 
 class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
         setContentView(R.layout.activity_settings)
     }
 }
