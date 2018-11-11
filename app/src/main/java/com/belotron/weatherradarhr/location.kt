9efe4c9bab29e3f1cfd751b7f4a1b194447cb842
Marko Topolnik <marko.topolnik@gmail.com>
2018-11-11 15:48:40 +0100

    Location to screen coords conversion (ARSO)

--- app/src/main/java/com/belotron/weatherradarhr/location.kt
+++ app/src/main/java/com/belotron/weatherradarhr/location.kt
@@ -1,61 +1,62 @@
 package com.belotron.weatherradarhr
 
+val lradarShape = ScreenShape(
+        topLat = 47.40,
+        botLat = 44.71,
+        topLeftLon = 12.10,
+        botLeftLon = 12.19,
+        topRightLon = 17.40,
+        botRightLon = 17.30,
+        leftScreenX = 10,
+        rightScreenX = 810,
+        topScreenY = 49,
+        botScreenY = 649
+)
+
 val kradarShape = ScreenShape(
-        topLat = Lat(48.07), // 47.89, 48.02
-        botLat = Lat(43.37), // 43.38, 43.36
-        topLeftLon = Lon(13.06),
-        botLeftLon = Lon(13.15),
-        topRightLon = Lon(19.86),
-        botRightLon = Lon(19.76)
+        topLat = 48.06,
+        botLat = 43.44,
+        topLeftLon = 13.05,
+        botLeftLon = 13.15,
+        topRightLon = 19.84,
+        botRightLon = 19.74,
+        leftScreenX = 1,
+        rightScreenX = 478,
+        topScreenY = 1,
+        botScreenY = 478
 )
 
-fun toScreenCoords(
-        lat: Lat, lon: Lon,
-        expectedScreenX: Int, expectedScreenY: Int,
-        screenShape: ScreenShape
-): Pair<Int, Int> = with(screenShape) {
-    val midLon = (topLeftLon + topRightLon) / 2.0.Lon
-    val x = lon - midLon
-    val y = topLat - lat
+class ScreenShape(
+        val topLat: Double,
+        val botLat: Double,
+        val topLeftLon: Double,
+        topRightLon: Double,
+        botLeftLon: Double,
+        botRightLon: Double,
+        leftScreenX: Int,
+        rightScreenX: Int,
+        val topScreenY: Int,
+        botScreenY: Int
+) {
+    // zeroLon satisfies the following:
+    // (zeroLon - topLeftLon) / (topRightLon - zeroLon) ==
+    // (zeroLon - botLeftLon) / (botRightLon - zeroLon)
+    val zeroLon: Double = (topLeftLon * botRightLon - botLeftLon * topRightLon) /
+            (topLeftLon - botLeftLon + botRightLon - topRightLon)
+    val screenWidth: Int = rightScreenX - leftScreenX
+    val xScaleAtTop: Double = screenWidth / (topRightLon - topLeftLon)
+    val xScaleAtBot: Double = screenWidth / (botRightLon - botLeftLon)
+    val zeroScreenX: Double = 0.5 + leftScreenX + xScaleAtTop * (zeroLon - topLeftLon)
+    val screenHeight: Int = botScreenY - topScreenY
 
-    val normY = y / (topLat - botLat)
-
-    val xScaleAtTop = 1 / (topRightLon - midLon).lon
-    val xScaleAtBottom = 1 / (botRightLon - midLon).lon
-    val xScaleAtY = (xScaleAtTop + (xScaleAtBottom - xScaleAtTop) * normY.lat).Lon
-    val normX = x * xScaleAtY
-
-    val screenX = (240 * (1 + normX.lon)).toInt()
-    val screenY = (480 * normY.lat).toInt()
-
-    println("($lon, $lat), ($normX, $normY): (${screenX - expectedScreenX}, ${screenY - expectedScreenY})")
-    return Pair(screenX, screenY)
+    fun toScreenCoords(
+            lat: Double, lon: Double
+    ): Pair<Int, Int> {
+        val normY: Double = (topLat - lat) / (topLat - botLat)
+        val xScaleAtY: Double = xScaleAtTop + (xScaleAtBot - xScaleAtTop) * normY
+        return Pair(
+                (zeroScreenX + xScaleAtY * (lon - zeroLon)).toInt(),
+                topScreenY + (screenHeight * normY).toInt()
+        )
+    }
 }
-
-data class ScreenShape(
-        val topLat: Lat,
-        val botLat: Lat,
-        val topLeftLon: Lon,
-        val topRightLon: Lon,
-        val botLeftLon: Lon,
-        val botRightLon: Lon
-)
-
-inline class Lat(val lat: Double) {
-    operator fun plus(that: Lat) = Lat(lat + that.lat)
-    operator fun minus(that: Lat) = Lat(lat - that.lat)
-    operator fun times(that: Lat) = Lat(lat * that.lat)
-    operator fun div(that: Lat) = Lat(lat / that.lat)
-    override fun toString() = "%04.2f".format(lat)
-}
-
-inline class Lon(val lon: Double) {
-    operator fun plus(that: Lon) = Lon(lon + that.lon)
-    operator fun minus(that: Lon) = Lon(lon - that.lon)
-    operator fun times(that: Lon) = Lon(lon * that.lon)
-    operator fun div(that: Lon) = Lon(lon / that.lon)
-    override fun toString() = "%04.2f".format(lon)
-}
-
-inline val Double.Lon get() = Lon(this)
-inline val Double.Lat get() = Lat(this)
