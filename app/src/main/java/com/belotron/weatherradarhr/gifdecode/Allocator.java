520b35a5c9fe1e6f66ac00d6a0ce97690b6fd4cd
Marko Topolnik <marko.topolnik@gmail.com>
2021-05-20 16:29:00 +0200

    Remove unused method

--- app/src/main/java/com/belotron/weatherradarhr/gifdecode/Allocator.java
+++ app/src/main/java/com/belotron/weatherradarhr/gifdecode/Allocator.java
@@ -47,7 +47,4 @@ public interface Allocator {
      * Release the given array back to the pool.
      */
     void release(@NonNull int[] array);
-
-    /** Disposes the bitmaps in the freelists and lets go of all the held objects. */
-    void dispose();
 }
