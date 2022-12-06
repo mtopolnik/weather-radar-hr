67893d653f79f563922d961bd0570f4698b4e487
Marko Topolnik <marko.topolnik@gmail.com>
2022-12-06 17:19:41 +0100

    Grey out disabled radars in Add/Remove

--- app/src/main/java/com/belotron/weatherradarhr/AddRemoveRadarActivity.kt
+++ app/src/main/java/com/belotron/weatherradarhr/AddRemoveRadarActivity.kt
@@ -10,8 +10,11 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+private const val VIEWTYPE_ITEM = 1
+private const val VIEWTYPE_DIVIDER = 2
+
 class AddRemoveRadarActivity: AppCompatActivity() {
-    private lateinit var adapter: ItemViewAdapter
+    private lateinit var items: Array<RadarSource?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,11 @@ class AddRemoveRadarActivity: AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.add_remove_recycler).also {
             it.layoutManager = LinearLayoutManager(this)
         }
-        adapter = ItemViewAdapter(mainPrefs.configuredRadarSources().toTypedArray())
+        items = mainPrefs.configuredRadarSources().toTypedArray()
+        val adapter = ItemViewAdapter(recyclerView, items,
+            resources.getColor(R.color.text_primary),
+            resources.getColor(R.color.text_disabled)
+        )
         recyclerView.adapter = adapter
         ItemTouchHelper(ItemMoveCallback(adapter)).attachToRecyclerView(recyclerView)
     }
@@ -29,23 +36,35 @@ class AddRemoveRadarActivity: AppCompatActivity() {
         super.onPause()
         info { "AddRemoveRadarActivity.onPause" }
         mainPrefs.applyUpdate {
-            setConfiguredRadarSources(adapter.items.toList())
+            setConfiguredRadarSources(items.toList())
         }
     }
 }
 
 class ItemViewAdapter(
-    val items: Array<RadarSource?>
+    private val recyclerView: RecyclerView,
+    private val items: Array<RadarSource?>,
+    private var textColorEnabled: Int,
+    private var textColorDisabled: Int
 ) : RecyclerView.Adapter<ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_add_remove, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            if (viewType == VIEWTYPE_ITEM) R.layout.add_remove_recycler_item else R.layout.add_remove_divider,
+            parent, false
+        )
         return ItemViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.radarNameView.text = items[position]?.run { title } ?: "==================="
+        items[position]?.also { holder.text = it.title }
+        val positionOfSeparator = items.indexOfFirst { it == null }
+        holder.textColor = if (holder.adapterPosition <= positionOfSeparator) textColorEnabled else textColorDisabled
+
     }
+
+    override fun getItemViewType(position: Int): Int =
+        if (items[position] != null) VIEWTYPE_ITEM else VIEWTYPE_DIVIDER
 
     override fun getItemCount() = items.size
 
@@ -60,11 +79,30 @@ class ItemViewAdapter(
         }
         items[to] = movedItem
         notifyItemMoved(from, to)
+        var textColor = textColorEnabled
+        items.indices.forEach { i ->
+            if (items[i] == null) {
+                textColor = textColorDisabled
+            } else {
+                (recyclerView.findViewHolderForAdapterPosition(i) as ItemViewHolder)
+                    .textColor = textColor
+            }
+        }
     }
 }
 
 class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val radarNameView = itemView.findViewById<TextView>(R.id.add_remove_item_text)!!
+    var text: String?
+        set(value) {
+            (itemView as? TextView)?.apply { text = value }
+        }
+        get() = (itemView as? TextView)?.run { text.toString() }
+
+    var textColor: Int
+        set(value) {
+            (itemView as? TextView)?.apply { setTextColor(value) }
+        }
+        get() = throw UnsupportedOperationException("Getting text color is not supported")
 }
 
 class ItemMoveCallback(
